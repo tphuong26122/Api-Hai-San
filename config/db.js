7d1e4d0e81fdee0b67d5697c3b2d7537f82400a1
tp <tphuong26122@gmail.com>
2025-04-17 15:16:23 +0700

    fix db

--- config/db.js
+++ config/db.js
@@ -1,14 +1,20 @@
 const mysql = require('mysql2');
+require('dotenv').config(); // Để đọc file .env
 
-const mysql = require('mysql2');
-const connection = mysql.createConnection(process.env.DATABASE_URL);
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+});
 
 connection.connect((err) => {
   if (err) {
     console.error('Lỗi kết nối MySQL:', err);
-  } else {
-    console.log('✅ Đã kết nối tới database Aiven MySQL!');
+    return;
   }
+  console.log('Kết nối MySQL thành công!');
 });
 
 module.exports = connection;
